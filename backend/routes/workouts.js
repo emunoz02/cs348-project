7c842f50a50e9c978ae54067a70c6400e0be4d9e
Endy <munoz121@purdue.edu>
2024-10-25 00:37:17 -0400

    Schema defined

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,29 +1,37 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const Workout = require("../models/WorkoutModel");
+const router = express.Router();
 
 //Get all workouts
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all workouts'})
-})
+router.get("/", (req, res) => {
+  res.json({ mssg: "GET all workouts" });
+});
 
 //Get single workouts
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET single workout'})
-})
+router.get("/:id", (req, res) => {
+  res.json({ mssg: "GET single workout" });
+});
 
 //POST single workouts
-router.post('/', (req, res) => {
-    res.json({mssg: 'POST single workout'})
-})
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+  res.json({ mssg: "POST single workout" });
+});
 
 //DELETE single workouts
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE single workout'})
-})
+router.delete("/:id", (req, res) => {
+  res.json({ mssg: "DELETE single workout" });
+});
 
 //UPDATE single workouts
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE single workout'})
-})
+router.patch("/:id", (req, res) => {
+  res.json({ mssg: "UPDATE single workout" });
+});
 
-module.exports = router
+module.exports = router;
