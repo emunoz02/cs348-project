f1367531226d6ef097b96ebc6290473c1b4a0ca6
Endy <munoz121@purdue.edu>
2024-10-24 20:11:59 -0400

    (4) connected to db and mongoose installed

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-
+const mongoose = require("mongoose");
 //express app
 const app = express();
-const workoutRoutes = require('./routes/workouts')
+const workoutRoutes = require("./routes/workouts");
 
 //middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -14,9 +14,17 @@ app.use((req, res, next) => {
 });
 
 //routes
-app.use('/api/workouts', workoutRoutes)
+app.use("/api/workouts", workoutRoutes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port 4000");
-});
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
